c1ea2ac16da82909af1e2b7205801cddce7784ad
co11aps <143988142+co11aps@users.noreply.github.com>
2024-07-07 23:44:05 +0800

    set up and generateContacts

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,16 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import * as fs from 'node:fs/promises';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+  let newContacts = [];
+  for (let i = 0; i < number; i++) {
+    newContacts.push(createFakeContact());
+  }
+  fs.readFile(PATH_DB, 'utf-8')
+    .then((data) => [...JSON.parse(data), ...newContacts])
+    .then((data) => fs.writeFile(PATH_DB, JSON.stringify(data, undefined, 2)))
+    .catch((error) => console.error(error));
+};
 
 generateContacts(5);
